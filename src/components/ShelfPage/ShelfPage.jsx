44a5c03c22971b9ad234a4cf259fcc8747385ba9
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-12 17:13:23 -0500

    added a comment for later

--- src/components/ShelfPage/ShelfPage.jsx
+++ src/components/ShelfPage/ShelfPage.jsx
@@ -20,6 +20,7 @@ function ShelfPage() {
           <img src={item.image_url} />
           <br />
           {item.description} posted by {item.user_id}
+          {/* conditionally render per user */}
           <button onClick={() => dispatch({type: 'DELETE_ITEM', payload: {id: item.id}})}>Delete</button>
         </li>
       ))}
