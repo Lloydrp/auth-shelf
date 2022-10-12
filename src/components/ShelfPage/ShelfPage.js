700465409f5fc72dc3642c93332005962b832aed
David Vetter <ddvetter23@gmail.com>
2022-10-12 16:01:17 -0500

    removed code in shelf

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import AddToShelf from '../AddToShelf/AddToShelf';
 
 function ShelfPage() {
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      <AddToShelf />
     </div>
   );
 }
