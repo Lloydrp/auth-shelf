c511c4ac7fa026ebbd4a381e47f1a9e27f2dfa24
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-13 09:34:08 -0500

    commented out console.logs in deleteItem saga

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -27,8 +27,8 @@ function* addToShelf(action) {
 
 function* deleteItem(action) {
   try {
-    yield console.log('in Delete Item');
-    yield console.log(action.payload);
+    // yield console.log('in Delete Item');
+    // yield console.log(action.payload);
     yield axios.delete(`/api/shelf/${action.payload.id}`);
     yield put({type: 'SET_SHELF_LIST'});
   } catch(err) {
